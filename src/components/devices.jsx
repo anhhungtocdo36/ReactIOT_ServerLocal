4a272f4decff9b6dda9f55c754cb4bb6bdb8e763
anhhungtocdo36 <loctranthanh36@gmail.com>
2017-12-15 14:02:23 +0700

    merge error

--- src/components/devices.jsx
+++ src/components/devices.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import axios from 'axios'
 import Chart from 'chart.js'
-import openSocket from 'socket.io-client';
+import openSocket from 'socket.io-client'
+import TimePicker from 'rc-time-picker'
+import moment from 'moment'
+import 'rc-time-picker/assets/index.css'
+
 const socket = openSocket(API_URL);
+const format = 'hh:mm a';
 
 export default class Devices extends React.Component {
     constructor(props) {
@@ -10,7 +15,6 @@ export default class Devices extends React.Component {
         this.devices = [];
         this.dataFetched = false;
         this.fetchData = this.fetchData.bind(this);
-        this.updateData = this.updateData.bind(this);
         this.goBack = this.goBack.bind(this);
         this.goForward = this.goForward.bind(this);
     }
@@ -18,13 +22,6 @@ export default class Devices extends React.Component {
     componentDidMount() {
         this.fetchData();
         document.title = "Thiết bị";
-        socket.on('switch', (data) => {
-            for(var i = 0; i < this.devices.length; i++) {
-                if(this.devices[i].id == data.ID)
-                    this.devices[i].status = parseInt(data.Status);
-            }
-            this.forceUpdate();
-        })
     }
 
     fetchData() {
@@ -44,11 +41,6 @@ export default class Devices extends React.Component {
             });
     }
 
-    updateData(message, data) {
-        socket.emit('switch', data);
-        console.log('Send HTTP: ' + JSON.stringify(data));
-    }
-
     goBack() {
         this.props.history.goBack();
     }
@@ -59,7 +51,7 @@ export default class Devices extends React.Component {
 
     render() {
         const item = this.devices.length ? this.devices.map((e, i) => 
-            <Device_item key={i} _id={e.id} name={e.name} status={e.status} power={{ value: e.value.reverse(), date: e.date.reverse()}} updateData={this.updateData}/>
+            <Device_item key={i} _id={e.id} name={e.name} status={e.status} time={e.time} timer_status={e.timer_status} power={{ value: e.value.reverse(), date: e.date.reverse()}} match={this.props.match} />
         ) : <div className="loading">{this.dataFetched ? "Không có thiết bị" : "Đang tải..."}</div>;
         return (
             <div>
@@ -79,20 +71,48 @@ export default class Devices extends React.Component {
 class Device_item extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { current: new Array(50).fill(0) }
-        this.switch = this.switch.bind(this);
+        this.state = { 
+            current: new Array(50).fill(0),
+            status: this.props.status
+        }
+        this.time = moment().hour(this.props.time.slice(0, 2)).minute(this.props.time.slice(3, 5)).second(this.props.time.slice(-2));
+        this.switch1 = this.switch1.bind(this);
+        this.switch2 = this.switch2.bind(this);
         this.drawCurrent = this.drawCurrent.bind(this);
         this.delete = this.delete.bind(this);
     }
 
-    switch(status) {
-        this.props.updateData('demo/switch', {
+    switch1(status) {
+        this.setState({ status });
+        socket.emit('switch', {
             ID: parseInt(this.props._id),
-            Status: status? '1':'0'
+            Status: status ? '1':'0'
         });
     }
+
+    switch2(status) {
+        var time = this.time.format(format);
+        if(time.slice(-2) == 'am') {
+            if(time.slice(0, 2) == '12') {
+                time = '00' + time.slice(2);
+            }
+        }
+        else {
+            if(time.slice(0, 2) != '12') {
+                time = (parseInt(time.slice(0, 2)) + 12) + time.slice(2);
+            }
+        }
+        time = time.slice(0, 5) + ':00';
+        socket.emit('timer', {
+            id: this.props._id,
+            time: time,
+            status
+        });
+    }
+
     componentDidMount() {
         let self = this;
+
         socket.on('current', (data) => {
             this.setState(prev => {
                 if (data.ID == this.props._id)
@@ -101,6 +121,12 @@ class Device_item extends React.Component {
                     return
             });
         })
+        socket.on('switch', (data) => {
+            if (self.props._id == data.ID) {
+                self.setState({ status: parseInt(data.Status) });
+            }
+        })
+
         var ctx = this.canvas2.getContext('2d');
         var labels = this.props.power.date.map((e, i) => {
             return this.props.power.date.length - i + ' days ago';
@@ -150,7 +176,7 @@ class Device_item extends React.Component {
                 datasets: [{
                     data: data,
                     fill: false,
-                    borderColor: '#ff0',
+                    borderColor: '#fff',
                     cubicInterpolationMode: 'monotone'
                 }]
             },
@@ -160,12 +186,12 @@ class Device_item extends React.Component {
                         type: 'linear',
                         position: 'bottom',
                         ticks: {
-                            fontColor: '#000',
+                            fontColor: '#fff',
                         },
                     }],
                     yAxes: [{
                         ticks: {
-                            fontColor: '#000',
+                            fontColor: '#fff',
                             beginAtZero: true,
                             suggestedMax: 10
                         },
@@ -182,12 +208,10 @@ class Device_item extends React.Component {
     }
 
     delete() {
-        axios.get(API_URL + 'delete/device/' + this.props._id).then(response => {
-            alert('Xóa thành công!');
+        axios.get(API_URL + 'delete/device/' + this.props._id + '/' + this.props.match.params.id).then(response => {
             window.location.reload();
         }).catch(err => {
-            alert('Xóa thất bại!');
-            window.location.reload();            
+            window.location.reload();
         })
     }
 
@@ -199,7 +223,21 @@ class Device_item extends React.Component {
                     <div className="row"><span>Tên : </span>{this.props.name}</div>
                     <div className="row">
                         <span>Trạng thái : </span>
-                        <Toggle on={this.props.status} switch={this.switch} />
+                        <Toggle on={this.state.status} switch={this.switch1} />
+                    </div>
+                    <div className="row">
+                        <span>Hẹn giờ : </span>
+                        <TimePicker
+                            showSecond={false}
+                            defaultValue={this.time}
+                            className="xxx"
+                            format={format}
+                            onChange={(value) => {
+                                this.time = value;
+                            }}
+                            use12Hours
+                        />
+                        <Toggle on={this.props.timer_status} switch={this.switch2} />                        
                     </div>
                     <div className="row" style={{textAlign: 'center'}}>Cường độ dòng</div>
                     <div className="row">
@@ -230,7 +268,7 @@ class Toggle extends React.Component {
     }
 
     componentWillReceiveProps(nextProps) {
-        if(nextProps != this.state.props)
+        if(nextProps.on != this.props.on)
             this.setState({ on: nextProps.on });
     }
 
